9c55dcc2127a85d08b49eb168af6beff1c5d3033
Lukesudom <lukemarshall511@gmail.com>
2022-06-15 17:55:45 +1000

    Added highscore (still work to do) no timer and local storage

--- game.js
+++ game.js
@@ -1,98 +1,113 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
-
-//Questions//
+let availableQuesions = [];
 
 let questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choice1: "strings",
+    choice2: "booleans",
+    choice3: "alerts",
+    choice4: "numbers",
+    answer: 3
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed within _____?",
+    choice1: "quotes",
+    choice2: "curly brackets",
+    choice3: "parathensis",
+    choice4: "square brackets",
+    answer: 3
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _____",
+    choice1: "numbers and strings",
+    choice2: "other arrays",
+    choice3: "booleans",
+    choice4: "all of the above",
+    answer: 4
+  },
+
     {
-        question: 'Inside which HTML element do we put the JavaScript??',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    choice1: "commas",
+    choice2: "curly brackets",
+    choice3: "quotes",
+    choice4: "parantheses",
+    answer: 3
+  }
 ];
 
-// Constants
-
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
 };
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
-        //go to end page
-        return window.location.assign("/end.html");
-    }
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("./end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    choices.forEach ( choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-    availableQuestions.splice(questionIndex, 1);
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-    acceptingAnswers = true;
-
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
 
-    choices.forEach(choice => {
-        choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset ["number"];
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-       const classToApply = 
-       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-       selectedChoice.parentElement.classList.add(classToApply);
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
 
+    selectedChoice.parentElement.classList.add(classToApply);
 
-       setTimeout( () => {
-       selectedChoice.parentElement.classList.remove(classToApply);
-       getNewQuestion();
-       }, 1000);
-      
-        });
-    });
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
 startGame();
-
-
-
-
